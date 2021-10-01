fc4cbd448089f06a59eeecc07c3693ba673db562
LPendelj <pendeljluka@gmail.com>
2021-10-01 18:25:33 +0200

    podizem aplikaciju za upload podataka na cloud

---
+++ BetterReadsDataLoader/src/main/java/rs/luka/BetterReadsDataLoader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package rs.luka.BetterReadsDataLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ BetterReadsDataLoader/src/test/java/rs/luka/BetterReadsDataLoader/BetterReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.luka.BetterReadsDataLoader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
